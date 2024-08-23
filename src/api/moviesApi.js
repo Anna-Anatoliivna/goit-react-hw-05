7a9e6102209aec15c887b34a0ffbffb58d9a3dac
Anna <iaanya27@gmail.com>
2024-08-23 11:02:27 +0300

    added fetch functions

--- src/api/moviesApi.js
+++ src/api/moviesApi.js
@@ -1,17 +1,37 @@
-import axios from 'axios';
+import Axios from 'axios';
 
-const url =
-  'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1';
+const tokenKey =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGI2MjY4MTU1YTVhZmJmYTI1OGZiNTY4NWEyYTVlMiIsIm5iZiI6MTcyNDE4Njk5Mi4xOTYyMzQsInN1YiI6IjY2YzRmYTIzMWVlNjUxOWYxZTk3MGUyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.doRMcTtbhhLACuwln4XgBaE8dDkWtm3KuZsClMiq5sY';
 
-const options = {
+const axios = Axios.create({
+  baseUrl: 'https://api.themoviedb.org/3',
   headers: {
-    // Замість api_read_access_token вставте свій токен
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMGI2MjY4MTU1YTVhZmJmYTI1OGZiNTY4NWEyYTVlMiIsIm5iZiI6MTcyNDE4Njk5Mi4xOTYyMzQsInN1YiI6IjY2YzRmYTIzMWVlNjUxOWYxZTk3MGUyMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.doRMcTtbhhLACuwln4XgBaE8dDkWtm3KuZsClMiq5sY',
+    Authorization: `Bearer ${tokenKey}`,
+    accept: 'aplication/json',
   },
+});
+const apiPath = {
+  trend: '/trending/movie/day?',
+  movie: '/movie',
+  search: '/search/movie',
 };
 
-axios
-  .get(url, options)
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+export const fetchTrendMovie = async () => {
+  const response = await axios.get(apiPath.trend, {});
+  return response.data;
+};
+
+export const fetchSearchMovie = async (query, page = 1) => {
+  const response = await axios.get(apiPath.search, {
+    params: {
+    query,
+      page,
+    },
+  });
+  return response.data;
+};
+
+// axios
+//   .get(url, options)
+//   .then(response => console.log(response))
+//   .catch(err => console.error(err));
