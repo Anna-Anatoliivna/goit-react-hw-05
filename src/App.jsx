7ae5f54cca5cc6267b306f4d49044d9ca6f6fe62
Anna <iaanya27@gmail.com>
2024-08-24 12:08:56 +0300

    added all pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  // Navigate,
+  Route, Routes
+} from 'react-router-dom';
 import './App.css'
-import { lazy, Suspense } from 'react';
-import { Loader } from './components/Loader/Loader';
+// import { lazy, Suspense } from 'react';
+// import { Loader } from './components/Loader/Loader';
 import { Header } from './components/Header/Header';
+import HomePage from './pages/HomePage';
+import MoviesPage from './pages/MoviesPage';
+import MovieDetailsPage from './pages/MovieDetailsPage';
+import MovieCast from './components/MovieCast/MovieCast';
+import MovieReviews from './components/MovieReviews/MovieReviews';
 
-const HomePage = lazy(() => import('pages/HomePage'));
-const MoviesPage = lazy(() => import('pages/MoviesPage'));
-const MovieDetailsPage = lazy(() => import('pages/MovieDetailsPage'));
+// const HomePage = lazy(() => import('pages/HomePage'));
+// const MoviesPage = lazy(() => import('pages/MoviesPage'));
+// const MovieDetailsPage = lazy(() => import('pages/MovieDetailsPage'));
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={<Loader />}>
+      {/* <Suspense fallback={<Loader />}> */}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movies" element={<MoviesPage />} />
           <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-          <Route path="*" element={<Navigate to="/" />} />
+        <Route path="cast" element={<MovieCast />} />
+        <Route path="reviews" element={<MovieReviews />} />
+          {/* <Route path="*" element={<Navigate to="/" />} /> */}
         </Routes>
-      </Suspense>
+      {/* </Suspense> */}
     </>
   );
 };
